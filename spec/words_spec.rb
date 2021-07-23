1c29bf15136306876cd177d077f9723a3c985c61
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-23 11:20:04 -0700

    add passing test for clearing all words

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -23,4 +23,15 @@ describe('#Word' ) do
       expect(Word.all).to(eq([word, word2]))
     end
   end
+
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new("Hello", nil)
+      word.save()
+      word2 = Word.new("Hola", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
 end
