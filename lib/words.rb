66d7ffc296d3ee1f4f05efe9851484442c594d0d
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-23 11:18:03 -0700

    add passing test for saving albums

--- lib/words.rb
+++ lib/words.rb
@@ -4,8 +4,9 @@ class Word
   @@words = {}
   @@total_rows = 0
 
-  def initialize(word)
+  def initialize(word, id)
     @word = word
+    @id = id || @@total_rows += 1
   end
 
   def self.all 
@@ -15,5 +16,14 @@ class Word
   def save
     @@words[self.id] = Word.new(self.word, self.id)
   end
-  
+
+  def ==(word_to_compare)
+    self.word() == word_to_compare.word()
+  end
+
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+
 end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -2,8 +2,25 @@ require 'rspec'
 require 'pry'
 require 'words'
 
-describe('.all') do
-  it("returns an empty array when there are no words") do
-    expect(Word.all).to(eq([]))
+describe('#Word' ) do
+
+  before(:each) do
+    Word.clear()
+  end
+
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("Hello", nil)
+      word.save()
+      word2 = Word.new("Hola", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
   end
 end
