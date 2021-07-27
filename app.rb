dac8ffef636fba0aa14a14aa7e77d26bea6a8911
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-26 22:02:51 -0700

    add route to post new word and be able to see it on the home page

--- app.rb
+++ app.rb
@@ -2,11 +2,27 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/words')
-# require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do 
   @words = Word.all
   erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+post('/words') do
+  word = params[:new_word]
+  new_word = Word.new(word, nil)
+  new_word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
 end
