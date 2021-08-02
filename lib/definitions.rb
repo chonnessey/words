4b6c520b19b5eb807739f1c6e31f0bda27bec226
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-01 21:05:08 -0700

    add passing test for equality in definitions

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -5,10 +5,53 @@ class Definition
   @@definitions = {}
   @@total_rows = 0
 
-  def initialize(definition, id)
+  def initialize(definition, word_id, id)
     @definition = definition
+    @word_id = word_id
     @id = id || @@total_rows += 1
   end
 
-  
+  def ==(definition_to_compare)
+    (self.definition() == definition_to_compare.definition()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def self.all
+    @@definitions.values
+  end
+
+  def save
+    @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def update(definition, word_id)
+    self.definition = definition
+    self.word_id = word_id
+    @@definitions[self.id] = Definition.new(self.definition, self.word_id, self.id)
+  end
+
+  def delete
+    @@defintions.delete(self.id)
+  end
+
+  def self.clear
+    @@definitions = {}
+  end
+
+  def self.find_by_word(w_id)
+    definitions = []
+    @@definitions.values.each do |definition|
+      if definition.word_id == w_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
+  def word
+    Word.find(self.word_id)
+  end
 end
--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -1,3 +1,21 @@
 require 'rspec'
 require 'pry'
-require 'words'
+require 'definitions'
+
+describe '#Definitions' do
+
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word= Word.new("Defeat", nil)
+    @word.save()
+  end
+
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition = Definition.new("To lose", @word.id, nil)
+      definition2 = Definition.new("To lose", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
+end
