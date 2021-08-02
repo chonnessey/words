b849435a3551aa6a00e33c31ffd9fd1bf503132c
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-01 23:23:25 -0700

    add view and routes to edit a word

--- app.rb
+++ app.rb
@@ -32,3 +32,22 @@ get ('/words/:id') do
   @word = Word.find(params[:id].to_i())
   erb(:word)
 end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:word])
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
